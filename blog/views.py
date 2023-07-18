f5bb2855343ba0e998d6a8b07efbce16c3c1ccb0
Jisung Smart <jisung.smart@gmail.com>
2023-07-18 14:05:16 +0900

    add tag function to post list and detail

--- blog/views.py
+++ blog/views.py
@@ -33,15 +33,17 @@ def category_page(request, slug):
         category = Category.objects.get(slug=slug)
         post_list = Post.objects.filter(category=category)
 
-    return render(
-        request,
-        'blog/post_list.html',
-        {
+    context =  {
             'post_list': post_list,
             'categories': Category.objects.all(),
             'no_category_post_count': Post.objects.filter(category=None).count(),
             'category': category,
         }
+
+    return render(
+        request,
+        'blog/post_list.html',
+        context
     )
 
 
