b7fc82257da7fd258748f6b97b0251bbe60929f9
Jisung Smart <jisung.smart@gmail.com>
2023-07-18 14:17:30 +0900

    Add tag pages

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
@@ -46,7 +46,20 @@ def category_page(request, slug):
         context
     )
 
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
 
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+        }
+    )
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
